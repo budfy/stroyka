f7d2c78f0b5f07930d829cd2fe4afcabee42f9c6
Roman Serdiuk <51324307+budfy@users.noreply.github.com>
2022-05-16 20:18:34 +0300

    Mobile - 2

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -2,7 +2,6 @@ function mobileSize() {
   const mobile = document.querySelector('.mobile-page');
   if (window.matchMedia('(max-width: 991px)').matches && mobile) {
     mobile.style.height = mobile.offsetWidth * 1.9 + "px";
-    mobile.style.maxWidth = mobile.offsetHeight / 1.9 + "px";
   }
 }
 mobileSize();
